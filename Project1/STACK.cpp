2b72ca1cd4a749ae71a69d983027ea7a9a62f659
Clownier <Clownier.97@gmail.com>
2017-10-16 20:04:17 +0800

    initial stack

---
+++ Project1/STACK.cpp
@@ -0,0 +1,94 @@
+#include "STACK.h"
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#define NDEBUG
+#include <assert.h>
+
+void initSTACK(STACK * const p, int m)
+{
+	//assert(p);
+	p->elems = (int *)malloc(m * sizeof(int));
+	if (NULL == p->elems) 
+		exit(-1);
+	p->max = m;
+	p->pos = 0;
+}
+
+void initSTACK(STACK * const p, const STACK & s)
+{
+	if (NULL == s.elems) 
+		exit(-1);
+	p->elems = s.elems;
+	p->max = s.max;
+	p->pos = s.pos;
+}
+
+int size(const STACK * const p)
+{
+	if (NULL == p)
+		return -1;
+	return p->max;
+}
+
+int howMany(const STACK * const p)
+{
+	if (NULL == p || p->pos <= 0)
+		return 0;
+	return p->pos;
+}
+
+int getelem(const STACK * const p, int x)
+{
+	if (NULL == p || p->pos <= x)
+		return -1;
+	return p->elems[x];
+}
+
+STACK * const push(STACK * const p, int e)
+{
+	if (NULL == p)
+		exit(-1);
+	if (p->max <= p->pos) {
+		p -> elems = (int *)realloc(p->elems, (p->max + 5) * sizeof(int));
+		if (NULL == p->elems)
+			exit(-1);
+		p->max += 5;
+	}
+	p->elems[p->pos] = e;
+	p->pos++;
+	return p;
+}
+
+STACK * const pop(STACK * const p, int & e)
+{
+	if (NULL == p->elems|| 0 == p->pos)
+		exit(-1);
+	e = p->elems[--(p->pos)];
+	return p;
+}
+
+STACK * const assign(STACK * const p, const STACK & s)
+{
+	if (NULL == p)
+		exit(-1);
+	p->elems = s.elems;
+	p->max = s.max;
+	p->pos = s.pos;
+	return p;
+}
+
+void print(const STACK * const p)
+{
+	int i = 0;
+	for (; i < p->pos; i++) {
+		printf("%d\t", p->elems[i]);
+	}
+}
+
+void destroySTACK(STACK * const p)
+{
+	free(p->elems);
+	p->max = 0;
+	p->pos = 0;
+}
