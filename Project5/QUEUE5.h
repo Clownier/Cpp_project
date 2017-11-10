07a56bb7a3881cd21404943a4961baa2ddd1eeda
Clownier <Clownier.97@gmail.com>
2017-11-10 16:04:52 +0800

    完成实验四，通过测试

--- Project5/QUEUE5.h
+++ Project5/QUEUE5.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <malloc.h>
 #include <iostream>
 #include <stdio.h>
@@ -8,14 +9,13 @@
 #include <string>
 #include<cstring>
 #include <sstream> 
-
+#include"STACK3.h"
 
 class QUEUE {
-	int  *const  elems;	//申请内存用于存放队列的元素
-	const  int   max;	//elems能存放的最大元素个数
-	int   head, tail;		//队列头和尾，队列空时head=tail;初始时head=tail=0
+	STACK s1, s2;
+	STACK &inStack = s1, &outStack = s2;
 public:
-	QUEUE(int m);		//初始化循环队列：elems有m个元素
+	QUEUE(int m); //每个栈最多m个元素，要求实现的队列最多能入2m个元素
 	QUEUE(const QUEUE&s); 			//用队列s拷贝初始化队列
 	virtual operator int() const;			//返回队列的实际元素个数
 	virtual int full() const;		       //返回队列是否已满，满返回1，否则返回0
@@ -23,7 +23,7 @@ public:
 	virtual QUEUE& operator<<(int e); 	//将e入队列,并返回队列
 	virtual QUEUE& operator>>(int &e);	//出队列到e,并返回队列
 	virtual QUEUE& operator=(const QUEUE&s); //赋s给队列,并返回被赋值的队列
-	virtual void print() const;			//打印队列
 	std::string getprint() const;
+	virtual void print() const;			//打印队列
 	virtual ~QUEUE();					//销毁队列
 };
